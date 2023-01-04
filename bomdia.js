05b706da0d5a68f2e4e1c450d589ba54eca50e55
amanda bacchet <113456487+Amandabacchet@users.noreply.github.com>
2023-01-04 20:42:20 -0300

    Add files via upload

---
+++ bomdia.js
@@ -0,0 +1,11 @@
+let hora = 10;
+
+if( hora < 12 && hora > 6 ){
+ console.log("bom dia")
+}
+else if( hora >= 12 && hora <= 17){
+console.log("boa tarde")
+}
+else{
+    console.log("boa noite")
+}
---
+++ testevar.js
@@ -0,0 +1,21 @@
+// declaraçao das variaveis
+let ano = 2004;
+let idade = 18;
+let nome = amanda;
+let diadenascimento = 16;
+let nundasorte = 13;
+let familia = [agner, radzikoski, bacchet]; // array
+let job = [programadora, DevOps, empresaria, economista]; // array
+
+console.log(familia[2]);
+
+// outro sistema
+let corsistema = branco;
+
+function resetacor(){
+    corsistema();
+};
+console.log (corsistema);
+resetacor();
+console.log(corsistema);
+
